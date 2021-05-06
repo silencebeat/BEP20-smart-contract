3dcc33d827b8226a2b9c3e3ddfae9b501938843d
candra <candra@tokocrypto.com>
2021-05-06 13:53:34 +0700

    update

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,5 @@
-const TKOContract = artifacts.require("TKOToken");
+const TestContract = artifacts.require("TestToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(TKOContract);
+  deployer.deploy(TestContract);
 };
